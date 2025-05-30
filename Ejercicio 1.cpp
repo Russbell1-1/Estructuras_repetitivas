bf102b5a7faf07af8799c0055daa8c2d9cf4b5bf
Russbell1-1 <zvalkiria@outlook.es>
2025-05-30 00:28:47 -0500

    Agrega ejercicio 1 de estructuras repetitivas

---
+++ Ejercicio 1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a,n,i;
+	cout<<"Ingrese la cantidad de multiplos que desea: ";
+	cin>>n;
+		for(i=1; i<=n ; i++){
+		a=7*i;
+		cout<<a<<endl;
+		}
+	
+
+	return 0;
+}
