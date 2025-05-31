274611790a4dec3465aae6be761fb231b174ac9e
Russbell1-1 <zvalkiria@outlook.es>
2025-05-30 23:30:21 -0500

    Agrega ejercicio 2 de estructuras repetitivas

---
+++ ejercicio 2.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+int main (){
+	int n;
+	do {
+		cout<<"Ingrese el numero: ";
+		cin>>n;
+	} while (n>0);
+	cout <<"Fin de la captura";
+	return 0;
+	}
+
