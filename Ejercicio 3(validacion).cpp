e8eb1d3a62a6b5bb4d3be55544278be4e99b31eb
Russbell1-1 <zvalkiria@outlook.es>
2025-05-31 23:34:46 -0500

    Actualiza ejercicio 3 de estructuras repetitivas agrega validacion de entrada

--- Ejercicio 3(validacion).cpp
+++ Ejercicio 3(validacion).cpp
@@ -5,10 +5,13 @@ int main(){
 	int n, acum=0;
 	cout<<"Ingresa el numero limite: ";
 	cin>>n;
+	if(n<0){
+		cout<<"Numero no natural";
+	} else {
 		for (int i=1;i<=n;i++){
 		acum= acum + i;
 		cout<<"El resultado es: "<<acum<<endl;
 	    }	
-	
+	}
 	return 0;
 }
