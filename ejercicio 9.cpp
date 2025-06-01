91d2ca8a1823492c9831d03bb8c73a2f130828d2
Russbell1-1 <zvalkiria@outlook.es>
2025-06-01 14:03:39 -0500

    Agrega ejercicio 9 de estructuras repetitivas

---
+++ ejercicio 9.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h> // numeros positivos
+using namespace std; 
+
+int main(){
+	int n, aux=0;
+	cout<<"Ingrese el numero: ";
+	cin>>n;
+	while (n>0){
+		aux= aux + 1;
+		n=n/10;
+	}
+	cout<<"La cantidad de digitos es: "<<aux<<endl;
+	return 0;
+}
