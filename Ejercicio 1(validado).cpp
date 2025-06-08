99f1d8ff4f0c3c50487e9c4bc4398a8103bc008e
josue <josue.mlpro.2014@gmail.com>
2025-06-08 12:38:32 -0500

    descripcion de lo que se va a mostrar en pantalla

--- Ejercicio 1(validado).cpp
+++ Ejercicio 1(validado).cpp
@@ -5,11 +5,15 @@ int main(){
 	int a,n,i;
 	cout<<"Ingrese la cantidad de multiplos que desea: ";
 	cin>>n;
+	
+	cout<<"los primeros multiplos de 7 son: "<<endl;
+	
 	if (n<0){
 		cout<<"Cantidad no valida";
 	} else {
 		for(i=1; i<=n ; i++){
 		a=7*i;
+		
 		cout<<a<<endl;
 		}
 	}
